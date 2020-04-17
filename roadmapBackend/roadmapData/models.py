1e8e74eb5c70d3134e50ace639ac77b0dc6f29f0
blackbird52 <xinzezhang@outlook.com>
2020-04-17 23:08:46 +0800

    Eliminate warning:null has no effect on ManyToManyField

--- roadmapBackend/roadmapData/models.py
+++ roadmapBackend/roadmapData/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Article(models.Model):
-    article_references = models.ManyToManyField("self", blank=True, null=True)
+    article_references = models.ManyToManyField("self", blank=True)
 
     title = models.CharField(max_length=200, blank=True, default='')
     author = models.CharField(max_length=200, blank=True, default='')
@@ -15,23 +15,23 @@ class Article(models.Model):
 
 
 class ReadRecord(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    article = models.OneToOneField(Article, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    article = models.OneToOneField(Article, on_delete=models.CASCADE, blank=True)
 
     read_state = models.BooleanField(blank=True, default=False)
     text = models.TextField(blank=True, default='')
 
 
 class Essay(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
 
     text = models.TextField(blank=True, default='')
 
 
 class RoadMap(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    articles = models.ManyToManyField(Article, blank=True, null=True)
-    essays = models.ManyToManyField(Essay, blank=True, null=True)
-    road_maps = models.ManyToManyField("self", blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    articles = models.ManyToManyField(Article, blank=True)
+    essays = models.ManyToManyField(Essay, blank=True)
+    road_maps = models.ManyToManyField("self", blank=True)
 
     text = models.TextField(blank=True, default='')
