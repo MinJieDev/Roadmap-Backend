9a835671aa0a2214ab479fb4b00eb97c8b0ac93f
Mist <hdl730@163.com>
2020-04-30 01:21:54 +0800

    Hdl  add user model mixins (#34)
    * add user model mixins
    
    * fix: using user pk
    
    * refactor and fix error
    
    * fix permission denied
    
    * complete curd mixins
    
    * upd auth of feedback and user view

---
+++ roadmapBackend/roadmapData/utils.py
@@ -0,0 +1,81 @@
+from rest_framework import mixins, exceptions, status
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+
+
+class UserRetrieveModelMixin(mixins.RetrieveModelMixin):
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        user = request.user
+        if instance.user != user:
+            raise exceptions.PermissionDenied()
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
+class UserListModelMixin(mixins.ListModelMixin):
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        user = request.user
+        queryset = queryset.filter(user=user)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class UserCreateModelMixin(mixins.CreateModelMixin):
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        data['user'] = request.user.id
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class UserUpdateModelMixin(mixins.UpdateModelMixin):
+    def update(self, request, *args, **kwargs):
+        user = request.user
+        instance = self.get_object()
+        if instance.user != user:
+            raise exceptions.PermissionDenied()
+
+        partial = kwargs.pop('partial', False)
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+
+class UserDestroyModelMixin(mixins.DestroyModelMixin):
+    def destroy(self, request, *args, **kwargs):
+        user = request.user
+        instance = self.get_object()
+        if instance.user != user:
+            raise exceptions.PermissionDenied()
+
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserModelViewSet(UserCreateModelMixin,
+                       UserRetrieveModelMixin,
+                       UserUpdateModelMixin,
+                       UserDestroyModelMixin,
+                       UserListModelMixin,
+                       GenericViewSet):
+    pass
--- roadmapBackend/roadmapData/views.py
+++ roadmapBackend/roadmapData/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, mixins
 from rest_framework import authentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework_jwt.serializers import jwt_payload_handler, jwt_encode_handler
 
@@ -8,8 +9,11 @@ from roadmapData import models
 from roadmapData import serializers
 from rest_framework.response import Response
 
+from roadmapData.utils import UserModelViewSet
 
-class UserViewSet(viewsets.ModelViewSet):
+
+class UserViewSet(mixins.CreateModelMixin,  # only CREATE is permitted
+                  GenericViewSet):
     queryset = models.User.objects
     serializer_class = serializers.UserSerializer
 
@@ -33,28 +37,25 @@ class UserViewSet(viewsets.ModelViewSet):
         return serializer.save()
 
 
-class ArticleViewSet(viewsets.ModelViewSet):
+class ArticleViewSet(UserModelViewSet):
     queryset = models.Article.objects
     serializer_class = serializers.ArticleSerializer
     permission_classes = (IsAuthenticated,)
 
 
-class EssayViewSet(viewsets.ModelViewSet):
+class EssayViewSet(UserModelViewSet):
     queryset = models.Essay.objects
     serializer_class = serializers.EssaySerializer
     permission_classes = (IsAuthenticated,)
 
 
-class RoadMapViewSet(viewsets.ModelViewSet):
+class RoadMapViewSet(UserModelViewSet):
     queryset = models.RoadMap.objects
     serializer_class = serializers.RoadMapSerializer
     permission_classes = (IsAuthenticated,)
 
-    def list(self, request, *args, **kwargs):
-        serializer = serializers.RoadMapViewSetSerializer(self.queryset, many=True)
-        return Response(serializer.data)
 
-
-class FeedbackViewSet(viewsets.ModelViewSet):
+class FeedbackViewSet(mixins.CreateModelMixin,  # only CREATE is permitted
+                      GenericViewSet):
     queryset = models.Feedback.objects
     serializer_class = serializers.FeedbackSerializer
