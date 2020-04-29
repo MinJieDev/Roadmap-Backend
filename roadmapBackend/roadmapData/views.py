61828675c2e698f81bc0912ce3355257d292e06d
blackbird52 <xinzezhang@outlook.com>
2020-04-29 18:16:34 +0800

    add permission

--- roadmapBackend/roadmapData/views.py
+++ roadmapBackend/roadmapData/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework import authentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework_jwt.serializers import jwt_payload_handler, jwt_encode_handler
 
@@ -35,21 +36,25 @@ class UserViewSet(viewsets.ModelViewSet):
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = models.Article.objects
     serializer_class = serializers.ArticleSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class ReadRecordViewSet(viewsets.ModelViewSet):
     queryset = models.ReadRecord.objects
     serializer_class = serializers.ReadRecordSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class EssayViewSet(viewsets.ModelViewSet):
     queryset = models.Essay.objects
     serializer_class = serializers.EssaySerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class RoadMapViewSet(viewsets.ModelViewSet):
     queryset = models.RoadMap.objects
     serializer_class = serializers.RoadMapSerializer
+    permission_classes = (IsAuthenticated,)
 
     def list(self, request, *args, **kwargs):
         serializer = serializers.RoadMapViewSetSerializer(self.queryset, many=True)
@@ -59,3 +64,4 @@ class RoadMapViewSet(viewsets.ModelViewSet):
 class FeedbackViewSet(viewsets.ModelViewSet):
     queryset = models.Feedback.objects
     serializer_class = serializers.FeedbackSerializer
+    permission_classes = (IsAuthenticated,)
