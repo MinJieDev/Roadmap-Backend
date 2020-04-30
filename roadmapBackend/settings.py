5683f3da6c85983d0dc3dbc6c0f5b3dbf2df35ab
Mist <hdl730@163.com>
2020-04-30 15:05:11 +0800

    hdl: apply mysql server

--- roadmapBackend/settings.py
+++ roadmapBackend/settings.py
@@ -93,8 +93,11 @@ WSGI_APPLICATION = 'roadmapBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'db.conf'),
+            'init_command': 'SET default_storage_engine=INNODB',
+        }
     }
 }
 
