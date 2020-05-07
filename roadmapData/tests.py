5ed37f9f05fd878abb505cb69b908d0a9b635f25
zhuziyu-edward <zhuziyu.edward@gmail.com>
2020-05-07 23:21:15 +0800

    finish main model test

--- roadmapData/tests.py
+++ roadmapData/tests.py
@@ -35,7 +35,7 @@ class ModelTest(APITestCase):
         response = self.client.post("/api/login/", data, format='json')
         user_token = json.loads(response.content)['token']
         self.client.credentials(HTTP_AUTHORIZATION='JWT ' + user_token)
-        
+
         #post
         data = {
             "title": "my article", 
@@ -62,33 +62,89 @@ class ModelTest(APITestCase):
         self.assertEqual(response_content["pages"], 8)
 
         # delete
+        response = self.client.delete("/api/articles/1.json", format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        response = self.client.get("/api/articles/1.json", format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    '''
-    def test_article_post(self):
-        # check post
+    def test_essay_model(self):
+        # get token
         data = {
-            "title": "my book",
-            "author": "zzy",
-            "journal": "science",
-            "volume": 1,
-            "pages": 2,
-            "years": 1998,
-            "url": "http://www.baidu.com"
+            "password": "minjie",
+            "username": "zzy"
         }
-        response = self.client.post("/api/articles/", data, format='json')
+        response = self.client.post("/api/login/", data, format='json')
+        user_token = json.loads(response.content)['token']
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + user_token)
+        
+        #post
+        data = {
+            "title": "my essay", 
+            "text": "first essay"
+        }
+        response = self.client.post("/api/essays/", data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        # check get
-        response = self.client.get("/api/articles/1.json", format='json')
-        self.assertEqual(response.status_code, 200)
-
-        # check content
+        # get
+        response = self.client.get("/api/essays/1.json", format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_content = json.loads(response.content)
-        self.assertEqual(response_content["title"], "my book")
-        self.assertEqual(response_content["author"], "zzy")
-        self.assertEqual(response_content["journal"], "science")
-        self.assertEqual(response_content["volume"], 1)
-        self.assertEqual(response_content["pages"], 2)
-        self.assertEqual(response_content["years"], 1998)
-        self.assertEqual(response_content["url"], "http://www.baidu.com")
-        '''
+        self.assertEqual(response_content["title"], "my essay")
+        self.assertEqual(response_content["text"], "first essay")
+
+        # update
+        data = {
+            "text": "12345", 
+        }
+        response = self.client.put("/api/essays/1.json", data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content["text"], "12345")
+
+        # delete
+        response = self.client.delete("/api/essays/1.json", format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        response = self.client.get("/api/essays/1.json", format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+
+    def test_road_map_model(self):
+        # get token
+        data = {
+            "password": "minjie",
+            "username": "zzy"
+        }
+        response = self.client.post("/api/login/", data, format='json')
+        user_token = json.loads(response.content)['token']
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + user_token)
+        
+        #post
+        data = {
+            "title": "my roadmap", 
+            "text": "pwd ss"
+        }
+        response = self.client.post("/api/road_maps/", data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response_content = json.loads(response.content)
+
+        # get
+        response = self.client.get("/api/road_maps/1.json", format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content["title"], "my roadmap")
+        self.assertEqual(response_content["text"], "pwd ss")
+
+        # update
+        data = {
+            "road_maps": ["1"]
+        }
+        response = self.client.put("/api/road_maps/1.json", data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content["road_maps"], [1])
+
+        # delete
+        response = self.client.delete("/api/road_maps/1.json", format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        response = self.client.get("/api/road_maps/1.json", format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
