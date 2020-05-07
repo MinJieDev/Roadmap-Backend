09e7ddece16bcb329b5fcd953b5f1cbc8af0a53b
zhuziyu-edward <zhuziyu.edward@gmail.com>
2020-05-07 22:57:31 +0800

    add model test

--- roadmapData/tests.py
+++ roadmapData/tests.py
@@ -5,30 +5,65 @@ from rest_framework.test import APIClient, APITestCase
 
 
 # Create your tests here.
-class BasicTest(APITestCase):
+class ModelTest(APITestCase):
     def setUp(self):
         self.client = APIClient()
-    
-    def test_user_post(self):
-        # check post
         data = {
-            "email": "zhuziyu.edward@gmail.com",
             "password": "minjie",
             "username": "zzy"
         }
         response = self.client.post("/api/users/", data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_user_model(self):
+        # get token
+        data = {
+            "password": "minjie",
+            "username": "zzy"
+        }
+        response = self.client.post("/api/login/", data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        user_token = json.loads(response.content)['token']
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + user_token)
+    
+    def test_article_model(self):
+        # get token
+        data = {
+            "password": "minjie",
+            "username": "zzy"
+        }
+        response = self.client.post("/api/login/", data, format='json')
+        user_token = json.loads(response.content)['token']
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + user_token)
         
-        # check get
-        response = self.client.get("/api/users/1.json", format='json')
-        self.assertEqual(response.status_code, 200)
-
-        # check content
+        #post
+        data = {
+            "title": "my article", 
+            "author": "edward"
+        }
+        response = self.client.post("/api/articles/", data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         response_content = json.loads(response.content)
-        self.assertEqual(response_content["username"], "zzy")
-        self.assertEqual(response_content["password"], "minjie")
-        self.assertEqual(response_content["email"], "zhuziyu.edward@gmail.com")
 
+        # get
+        response = self.client.get("/api/articles/1.json", format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content["title"], "my article")
+        self.assertEqual(response_content["author"], "edward")
+
+        # update
+        data = {
+            "pages": "8", 
+        }
+        response = self.client.put("/api/articles/1.json", data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_content = json.loads(response.content)
+        self.assertEqual(response_content["pages"], 8)
+
+        # delete
+
+    '''
     def test_article_post(self):
         # check post
         data = {
@@ -56,3 +91,4 @@ class BasicTest(APITestCase):
         self.assertEqual(response_content["pages"], 2)
         self.assertEqual(response_content["years"], 1998)
         self.assertEqual(response_content["url"], "http://www.baidu.com")
+        '''
