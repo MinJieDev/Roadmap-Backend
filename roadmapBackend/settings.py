166d9f8bd420df19ab26aa5e7189362954c8f00a
zhuziyu-edward <zhuziyu.edward@gmail.com>
2020-05-21 21:31:22 +0800

    change page size

--- roadmapBackend/settings.py
+++ roadmapBackend/settings.py
@@ -52,7 +52,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 2,
+    'PAGE_SIZE': 20,
 }
 
 JWT_AUTH = {
