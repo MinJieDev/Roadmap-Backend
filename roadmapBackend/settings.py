07fc0f019a2c04a0cf0cf3d8124f1bd786c48b2d
zhuziyu-edward <zhuziyu.edward@gmail.com>
2020-05-21 21:34:30 +0800

    chage page size

--- roadmapBackend/settings.py
+++ roadmapBackend/settings.py
@@ -52,7 +52,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 10,
 }
 
 JWT_AUTH = {
