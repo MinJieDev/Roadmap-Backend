ed6de59f24b87b2eea6782be785ea678c88aa41d
zhuziyu-edward <zhuziyu.edward@gmail.com>
2020-05-31 21:33:29 +0800

    change auth

--- roadmapData/views.py
+++ roadmapData/views.py
@@ -274,4 +274,20 @@ class GetNewpaperView(APIView):
 class CommentViewSet(UserModelViewSet):
     queryset = models.Comment.objects
     serializer_class = serializers.CommentSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = ()
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
